e28f409034ab6f0c758ccf145d946cae4fca7ec9
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-09 14:28:24 +0545

    hello

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const home = () => {
+    //js block
+return (
+    <div>
+                     homepage this is a main landing page. 
+    </div>
+)
+}
+
+export default home
