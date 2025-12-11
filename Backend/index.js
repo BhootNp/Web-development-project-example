ee10824362213df4925bc29113db85bd6e05bc4c
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-11 12:02:28 +0545

    helo

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
     res.json('Hello World from Backend!');
 });
 
+app.get('/auth/login', (req, res) => {
+    res.json('User logged in successfully!');
+});
+
+app.get('/auth/contact', (req, res) => {
+    res.json('Nice to hear from you! How can we help?');
+});
+
+app.get('/auth/user', (req, res) => {
+    res.json('Nice to meet you, User!');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
