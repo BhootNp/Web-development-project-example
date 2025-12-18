7fe892bbb6b4ca793e0725f8fd85ffe3d71572e7
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-18 12:08:37 +0545

    fixing

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,30 @@
-const getAllUsers = async(req, res) => {
-    res.json({ message: "Get all users" });
+const User = require('../models/userModel.js');
+
+const addUser = async (req, res) => {
+    try {
+        const { username, email, password} = req.body;
+        if (!username || !email || !password) {
+            return res.status(400).json({ 
+                message: "All fields are required" 
+            });
+        }
+        const newUser = await User.create({ 
+            username, 
+            email, 
+            password
+         });
+        
+        res.status(201).json({
+            message: "User added successfully",
+            user: newUser
+        });
+    } catch (error) {
+        res.status(500).json({ 
+            message: "Error adding user", 
+            error: error.message 
+        });
+    }
 };
 
-module.exports = { getAllUsers }
+
+module.exports = { addUser };
