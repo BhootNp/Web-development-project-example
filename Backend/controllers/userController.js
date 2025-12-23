948f95ac21ff3b00e54ab6619e3443d75db3d672
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-12-23 14:44:39 +0545

    hello

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,50 +1,112 @@
-const User = require('../models/userModel');
-const bscrypt = require('bcrypt');
+const User = require("../models/userModel");
+const bscrypt = require("bcrypt");
 
 const addUser = async (req, res) => {
-    try {
-        const { username, email, password} = req.body;
-        if (!username || !email || !password) {
-            return res.status(400).json({ 
-                message: "All fields are required" 
-            });
-        }
-        const isUser = await User.findOne({ where: { username } });
-       const isemail = await User.findOne({ where: { email } });
-         if (isUser || isemail) {
-                return res.status(400).json({ 
-                 message: "User already exists" 
-                });
-          }
-
-        const hassed = await bscrypt.hash(password, 10);
-        console.log(hassed);
-
-        const newUser = await User.create({ 
-            username, 
-            email, 
-            password : hassed
-         });
-        
-        res.status(201).json({
-            message: "User added successfully",
-            user: newUser
-        });
-    } catch (error) {
-        res.status(500).json({ 
-            message: "Error adding user", 
-            error: error.message 
-        });
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
     }
+    const isUser = await User.findOne({ where: { username } });
+    const isemail = await User.findOne({ where: { email } });
+    if (isUser || isemail) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hassed = await bscrypt.hash(password, 10);
+    console.log(hassed);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hassed,
+    });
+
+    res.status(201).json({
+      message: "User added successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error adding user",
+      error: error.message,
+    });
+  }
 };
 
 const getAllUsers = async (req, res) => {
-   const users =  User.findAll({});
-   res.json({users, message:"All users retrieved successfully"});
+  try {
+    const users = await User.findAll({ attributes: { exclude: ["password"] } });
+    res.status(200).json({ message: "Users retrieved successfully", users });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving users",
+      error: error.message,
+    });
+  }
 };
+
+const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id
+    const users = await User.findByPk(id)
+    if(!users){
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    return res.json({
+      user: { id: users.id, name: users.username },
+      message: "Users retrieved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error retrieving users",
+      error: error.message,
+    });
+  }
+};
+
+const updateUser=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const {username,email,password}=req.body;
+        const user=await User.findByPk(id);
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        if (username) {
+            const existingUser = await User.findOne({ where: { username } });
+            if (existingUser && existingUser.id !== user.id) {
+                return res.status(400).json({ 
+                    message: "Username already taken",})
+            }
+        }
+        let hassedPassword=user.password;
+        if(password){
+            hassedPassword=await bcrypt.hash(password,10);
+        }
+        await user.update({
+            username : username || user.username,
+            email : email || user.email,
+            password:hassedPassword
+
+        });
+        res.json({message:"User updated successfully",user});
+    }catch(error){
+        return res.status(500).json ({
+            message : "error updating user",
+            error : error.message,
+        });
+    }
+}
 
 const getActiveUsers = async (req, res) => {
-    res.json({ message: "Get active users - to be implemented" });
+  res.json({ message: "Get active users - to be implemented" });
 };
 
-module.exports = { addUser, getAllUsers , getActiveUsers };
+module.exports = { addUser, getAllUsers, getActiveUsers, getUserById , updateUser};
--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -1,9 +1,11 @@
 const express = require('express').Router();
 
-const {addUser, getActiveUsers, getAllUsers}= require('../controllers/userController');
+const {addUser,getActiveUsers,getAllUsers,getUserById, updateUser} = require('../controllers/userController');
 
 express.post('/register', addUser);
 express.get('/all', getAllUsers);
 express.get('/getactiveusers', getActiveUsers);
+express.get('/id/:id', getUserById);
+express.put('/update/:id',updateUser); 
 
 module.exports = express;
