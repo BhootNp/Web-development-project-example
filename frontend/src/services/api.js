be7f8572af7c2969b579dfaa55ad934ca0ca9ed4
Nelish Maharjan <nelishmaharjan16@icloud.com>
2026-01-04 10:32:35 +0545

    api and env

---
+++ frontend/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const ApiFormData = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'multipart/form-data',
+    },
+}); 
+
+const Api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
